b9ee93361aea1eb22d86bf6d8522814e3ea1546f
Sergey Mikhaylov <bodmode@gmail.com>
2020-12-20 17:26:31 +0300

    Stage 5 solved

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -2,25 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def capitalize(language_name):
+def capitalize(language_name: str) -> str:
     return language_name[0].upper() + language_name[1:]
 
 
-# languages = {'en': 'english', 'fr': 'french'}
+languages = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese', 'dutch', 'polish', 'portuguese',
+             'romanian', 'russian', 'turkish']
 
-print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English '
-      'into French:')
-target_language = input()
+print("Hello, you're welcome to the translator. Translator supports:")
+for i in range(len(languages)):
+    print(f'{i + 1}. {capitalize(languages[i])}')
+print('Type the number of your language: ')
+source = languages[int(input()) - 1]
+print('Type the number of language you want to translate to:')
+target = languages[int(input()) - 1]
 print('Type the word you want to translate:')
-word = input()
-print(f'You chose "{target_language}" as the language to translate "{word}" to.')
-
-if target_language == "fr":
-    source = "english"
-    target = "french"
-else:
-    source = "french"
-    target = "english"
+word = input().lower()
 
 translation_page = requests.get(f"https://context.reverso.net/translation/{source}-{target}/{word}",
                                 headers={'User-Agent': 'Mozilla/5.0'})
@@ -29,22 +26,17 @@ print(f"{translation_page.status_code} OK\n")
 soup = BeautifulSoup(translation_page.content, 'html.parser')
 
 words_translated = set(word.text.lower() for word in soup.find_all('a', {'class': 'link_highlighted'}))
-# examples_prc = [phrase.text.strip() for phrase in soup.find_all('div', {'class': 'trg ltr'})]
-examples_raw = [block for block in soup.find_all('div', {'class': 'example'})]
-examples = []
-for block in examples_raw:
-    print(block)
-    example = [block.find('div', {'class': 'src ltr'}).text.strip(),
-               block.find('div', {'class': 'trg ltr'}).text.strip()]
+examples_src = [phrase.text.strip() for phrase in soup.find_all('div', {'class': 'src ltr'})]
+examples_trg = [phrase.text.strip() for phrase in soup.find_all('div', {'class': 'trg ltr'})]
 
 print("Context examples:\n")
 print(f"{capitalize(target)} Translations:")
 for word in words_translated:
     print(word)
-# print(*words_translated, sep=', ')
 print()
+
 print(f"{capitalize(target)} Examples:")
-for example in examples:
-    print(example[0])
-    print(example[1])
-# print(*examples_prc, sep=', ')
+for i in range(len(examples_src)):
+    if i <= len(examples_trg) and i <= 5:
+        print(examples_src[i])
+        print(examples_trg[i])
