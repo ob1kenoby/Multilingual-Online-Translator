2f2e5540381dfc74e8c00b0d4f2deadf7b76b833
Sergey Mikhaylov <bodmode@gmail.com>
2020-12-29 21:46:49 +0300

    Stage 6 done

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,6 +1,7 @@
+import argparse
+import re
 import requests
 from bs4 import BeautifulSoup
-import re
 
 
 def capitalize(language_name: str) -> str:
@@ -26,27 +27,38 @@ def translate(source, target, word):
     return output
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("source")
+parser.add_argument("target")
+parser.add_argument("word")
+
+args = parser.parse_args()
+
+source = args.source
+target = args.target
+word = args.word
+
 languages = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese', 'dutch', 'polish', 'portuguese',
              'romanian', 'russian', 'turkish']
 
-print("Hello, you're welcome to the translator. Translator supports:")
-for i in range(len(languages)):
-    print(f'{i + 1}. {capitalize(languages[i])}')
-print('Type the number of your language: ')
-source = languages[int(input()) - 1]
-print("Type the number of language you want to translate to or '0' to translate to all languages:")
-choice = int(input())
-print('Type the word you want to translate:')
-word = input().lower()
+# print("Hello, you're welcome to the translator. Translator supports:")
+# for i in range(len(languages)):
+#     print(f'{i + 1}. {capitalize(languages[i])}')
+# print('Type the number of your language: ')
+# source = languages[int(input()) - 1]
+# print("Type the number of language you want to translate to or '0' to translate to all languages:")
+# choice = int(input())
+# print('Type the word you want to translate:')
+# word = input().lower()
 
 
 translated_text = ''
-if choice == 0:
+if target == "all":
     for i in range(len(languages)):
         if languages[i] != source:
             translated_text += translate(source, languages[i], word)
 else:
-    translated_text = translate(source, languages[choice - 1], word)
+    translated_text = translate(source, target, word)
 print(translated_text)
 f = open(f'{word}.txt', 'w')
 f.write(translated_text)
